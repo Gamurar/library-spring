2388d8452251345a806df2f6664a91343f4faa4c
Iana Gamurar <iana_gamurari@bk.ru>
2019-07-02 13:14:18 +0300

    add logout button

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +18,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MyUserDetailsService userDetailsService;
+
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/login", "/" ,"/home").permitAll()
+//                .antMatchers("/edit").hasRole("OPERATOR");
+//    }
 
 
     @Override
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO user(username, password)
+	VALUES('bcat', '$2a$11$FRPKwSA2cGYQkxaph5sOMOp..Ag.HkNmrdRp.1lSXHNpw0.oOIh4e');
+
+INSERT INTO author(name)
+    VALUES('Андрей Курпатов');
